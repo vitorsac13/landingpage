fe5c1898c6800487349d160526e0187b0b82f1a7
Vitor Cardoso <vitorsac13@gmail.com>
2026-01-28 18:51:07 -0300

    Configs

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,9 +2,22 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Home from './pages/home/page.jsx'
+
+const pages = createBrowserRouter([
+	{
+		path: '/',
+		element: <App />,
+		children: [
+			{ path: '/', element: <Home /> },
+			
+		]
+	}
+])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={pages}></RouterProvider>
   </StrictMode>,
 )
---
+++ frontend/src/pages/home/page.jsx
@@ -0,0 +1,10 @@
+import styles from './page.module.css'
+
+export default function Home(){
+
+    return(
+        <div>
+            <h1>Home Page</h1>
+        </div>
+    )
+}
