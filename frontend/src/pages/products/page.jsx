1e98745783b65a7a290b38e844a900e701d610e7
Vitor Cardoso <vitorsac13@gmail.com>
2026-01-29 16:51:25 -0300

    Update page Products

--- frontend/src/pages/products/page.jsx
+++ frontend/src/pages/products/page.jsx
@@ -1,17 +1,18 @@
 import styles from './page.module.css'
 import { FaPills } from "react-icons/fa"
+import generico from "../../assets/generico.png"
 
 export default function Products(){
 
     // Lista de medicamentos (mock local)
   const medicamentos = [
-    { nome: "Aripiprazol", dose: "10mg", qtd: "30 cápsulas" },
-    { nome: "Cloridrato de Venlafaxina", dose: "75mg", qtd: "30 cápsulas" },
-    { nome: "Cloridrato de Venlafaxina", dose: "150mg", qtd: "30 cápsulas" },
-    { nome: "Paracetamol", dose: "500mg", qtd: "30 cápsulas" },
-    { nome: "Dipirona", dose: "1g", qtd: "20 cápsulas" },
-    { nome: "Ibuprofeno", dose: "400mg", qtd: "10 cápsulas" },
-    { nome: "Maleato de Dexclorfeniramina", dose: "2mg", qtd: "20 cápsulas" },
+    { nome: "Aripiprazol", dose: "10mg", qtd: "30 cápsulas", img: generico},
+    { nome: "Cloridrato de Venlafaxina", dose: "75mg", qtd: "30 cápsulas", img: generico},
+    { nome: "Cloridrato de Venlafaxina", dose: "150mg", qtd: "30 cápsulas", img: generico},
+    { nome: "Paracetamol", dose: "500mg", qtd: "30 cápsulas", img: generico},
+    { nome: "Dipirona", dose: "1g", qtd: "20 cápsulas", img: generico},
+    { nome: "Ibuprofeno", dose: "400mg", qtd: "10 cápsulas", img: generico},
+    { nome: "Maleato de Dexclorfeniramina", dose: "2mg", qtd: "20 cápsulas", img: generico},
   ]
 
   return (
@@ -32,7 +33,7 @@ export default function Products(){
         <div className={styles.grid}>
           {medicamentos.map((med, index) => (
             <div key={index} className={styles.card}>
-              <FaPills className={styles.icon} />
+              <img src={med.img} alt={med.nome} className={styles.productImage} />
 
               <h3>{med.nome}</h3>
               <p><strong>Dosagem:</strong> {med.dose}</p>
